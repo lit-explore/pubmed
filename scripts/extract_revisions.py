00333a36519be3c5cd1a2851b99c4956592160c1
Keith Hughitt <keith.hughitt@gmail.com>
2022-12-25 08:07:08 -0800

    added script for extracting article revision info

---
+++ scripts/extract_revisions.py
@@ -0,0 +1,52 @@
+"""
+Parses Pubmed XML and extracts revision information for each article
+"""
+import os
+import gzip
+import xml.etree.ElementTree as ET
+from typing import List
+from datetime import datetime
+import pandas as pd
+
+with gzip.open(snakemake.input[0], "r") as fp:
+    tree = ET.parse(fp)
+
+root = tree.getroot()
+
+files:List[str] = []
+pmids:List[str] = []
+dates:List[str] = []
+
+# iterate over articles in xml file
+for article in root.findall(".//PubmedArticle"):
+    # extract pmid
+    pmid = article.find(".//ArticleId[@IdType='pubmed']").text
+
+    # exclude entries with malformed/non-numeric pubmed ids
+    if pmid is not None and not pmid.isnumeric():
+        continue
+
+    # extract revision date
+    rev_elem = article.find(".//DateRevised")
+
+    if rev_elem is None:
+        continue
+
+    year = rev_elem[0].text
+    month = rev_elem[1].text
+    day = rev_elem[2].text
+
+    DATE_FORMAT = "%Y %m %d" if month.isnumeric() else "%Y %b %d"
+
+    date_str = datetime.strptime(f"{year} {month} {day}", DATE_FORMAT).isoformat()
+
+    files.append(os.path.basename(snakemake.input[0]))
+    pmids.append(int(pmid))
+    dates.append(date_str)
+
+dat = pd.DataFrame({"id": pmids, "file": files, "date": dates})
+
+if dat.shape[0] == 0:
+    raise Exception("No articles found with all require components!")
+
+dat.reset_index(drop=True).to_feather(snakemake.output[0])
