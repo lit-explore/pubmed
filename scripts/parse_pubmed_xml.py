c7e59468b04771be117d3800e5155363a700f69b
Keith Hughitt <keith.hughitt@gmail.com>
2022-12-26 17:33:02 -0800

    fixed logic used to handle multiple article revisions

--- scripts/parse_pubmed_xml.py
+++ scripts/parse_pubmed_xml.py
@@ -2,6 +2,7 @@
 Parses Pubmed XML data and extract article pmid, title, and abstract information
 """
 import gzip
+import sys
 from datetime import datetime
 from typing import List
 import xml.etree.ElementTree as ET
@@ -28,6 +29,67 @@ abstracts:List[str] = []
 # max token length
 MAX_LEN = snek.config['tokens']['max_len']
 
+def extract_pub_date(article):
+    """
+    Extracts publication date for a single article
+    """
+    # extract publication date;
+    try:
+        date_elem = article.find(".//PubDate")
+
+        if date_elem is None:
+            raise Exception("Publication date not found! skipping..")
+
+        year_elem = date_elem.find("Year")
+
+        if year_elem is not None:
+            year = year_elem.text
+
+        month_elem = date_elem.find("Month")
+
+        if month_elem is not None:
+            month = month_elem.text
+        else:
+            # if no month found, default to january
+            month = "01"
+
+        day_elem = date_elem.find("Day")
+
+        if day_elem is not None:
+            day = day_elem.text
+        else:
+            # if no day found, default to the 1st
+            day = "01"
+
+        # check for numeric/string months
+        date_format = "%Y %m %d" if month.isnumeric() else "%Y %b %d"
+        date_str = datetime.strptime(f"{year} {month} {day}", date_format).isoformat()
+    except:
+        # if date parsing fails, just leave field blank
+        date_str = ""
+
+    return date_str
+
+def extract_revision_date(article):
+    """
+    Extracts revision date for a single article
+    """
+    # extract revision date
+    rev_elem = article.find(".//DateRevised")
+
+    if rev_elem is None:
+        return None
+
+    year = rev_elem[0].text
+    month = rev_elem[1].text
+    day = rev_elem[2].text
+
+    DATE_FORMAT = "%Y %m %d" if month.isnumeric() else "%Y %b %d"
+
+    date_str = datetime.strptime(f"{year} {month} {day}", DATE_FORMAT).isoformat()
+
+    return date_str
+
 # iterate over articles in xml file
 for article in root.findall(".//PubmedArticle"):
     # extract title
@@ -38,7 +100,8 @@ for article in root.findall(".//PubmedArticle"):
     else:
         title = title_elem.text.replace('\n', ' ').strip()
 
-    if snek.config['exclude_articles']['missing_title'] and title == "":
+    #  if snek.config['exclude_articles']['missing_title'] and title == "":
+    if title == "":
         continue
 
     # extract abstract
@@ -49,7 +112,8 @@ for article in root.findall(".//PubmedArticle"):
     else:
         abstract = abstract_elem.text.replace('\n', ' ').strip()
 
-    if snek.config['exclude_articles']['missing_abstract'] and abstract == "":
+    #  if snek.config['exclude_articles']['missing_abstract'] and abstract == "":
+    if abstract == "":
         continue
 
     # extract pmid
@@ -65,7 +129,6 @@ for article in root.findall(".//PubmedArticle"):
     else:
         continue
 
-
     # extract doi
     doi_elem = article.find(".//ArticleId[@IdType='doi']")
 
@@ -74,33 +137,17 @@ for article in root.findall(".//PubmedArticle"):
     else:
         doi = doi_elem.text.lower()
 
-    try:
-        date_elem = article.find(".//PubDate")
-
-        year = date_elem[0].text
-        month = date_elem[1].text
-
-        # if not day specified, default to the 1st
-        if len(date_elem) == 3:
-            day = date_elem[2].text
-        else:
-            day = "01"
-
-        # check for numeric/string months
-        date_format = "%Y %m %d" if month.isnumeric() else "%Y %b %d"
-
-        date_str = datetime.strptime(f"{year} {month} {day}", date_format).isoformat()
-    except:
-        # if date parsing fails, just leave field blank
-        date_str = ""
+    # extract publication date
+    pub_date = extract_pub_date(article)
 
     # if multiple revisions for an article are present, only keep the most recent one
     revs = revisions[revisions.id == pmid]
 
     if revs.shape[0] > 1:
         last_rev = revs.sort_values('date').tail(1).date.values[0]
+        rev_date = extract_revision_date(article)
 
-        if last_rev != date_str:
+        if last_rev != rev_date:
             continue
 
     # remove excessively long tokens
@@ -111,11 +158,12 @@ for article in root.findall(".//PubmedArticle"):
     dois.append(doi)
     titles.append(title)
     abstracts.append(abstract)
-    dates.append(date_str)
+    dates.append(pub_date)
 
 dat = pd.DataFrame({"id": pmids, "doi": dois, "title": titles, "abstract": abstracts, "date": dates})
 
 if dat.shape[0] == 0:
-    raise Exception("No articles found with all require components!")
+    print(f"Skipping {snek.input[0]}: no articles found with all needed pieces...")
+    sys.exit()
 
 dat.reset_index(drop=True).to_feather(snek.output[0])
